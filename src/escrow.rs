3bf74621faa24b0eb25dc584cc04400b79b59ef7
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2024-02-22 14:55:48 +0200

    add simple egld deposit/withdraw

--- src/escrow.rs
+++ src/escrow.rs
@@ -7,4 +7,19 @@ multiversx_sc::imports!();
 pub trait Escrow {
     #[init]
     fn init(&self) {}
+
+    #[payable("EGLD")]
+    #[endpoint(deposit)]
+    fn deposit(&self) {}
+
+    #[only_owner]
+    #[endpoint(deposit)]
+    fn withdraw(&self) {
+       let balance = &self
+            .blockchain()
+            .get_sc_balance(&EgldOrEsdtTokenIdentifier::egld(), 0);
+
+        self.send()
+            .direct_egld(&self.blockchain().get_caller(), balance);
+    }
 }
