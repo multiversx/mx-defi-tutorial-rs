fa79ad3b4b06efe0b349e69cc05203ffa7c3e6ac
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-08-07 13:48:34 +0300

    Removed target
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- target/debug/escrow-meta.d
+++
@@ -1,1 +0,0 @@
-/home/multiversx/mx-defi-tutorial-rs/target/debug/escrow-meta: /home/multiversx/mx-defi-tutorial-rs/meta/src/main.rs /home/multiversx/mx-defi-tutorial-rs/src/escrow.rs
--- target/wasm32-unknown-unknown/release/deps/escrow_wasm.d
+++
@@ -1,5 +0,0 @@
-/home/multiversx/mx-defi-tutorial-rs/target/wasm32-unknown-unknown/release/deps/escrow_wasm.wasm: src/lib.rs
-
-/home/multiversx/mx-defi-tutorial-rs/target/wasm32-unknown-unknown/release/deps/escrow_wasm.d: src/lib.rs
-
-src/lib.rs:
