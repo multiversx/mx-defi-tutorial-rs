09686dff72543c9390c03726a3e9f2692b5c09e9
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-08-07 13:47:47 +0300

    Removed everything from target
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- target/wasm32-unknown-unknown/release/escrow_wasm.d
+++
@@ -1,1 +0,0 @@
-/home/multiversx/mx-defi-tutorial-rs/target/wasm32-unknown-unknown/release/escrow_wasm.wasm: /home/multiversx/mx-defi-tutorial-rs/src/escrow.rs /home/multiversx/mx-defi-tutorial-rs/wasm/src/lib.rs
